d94c6379e2e54d3b58cefbc25bfce4bc0dfafa3f
Colin de Roos <colinderoos@gmail.com>
2019-08-05 11:34:10 +0200

    Add initial test configuration

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  coveragePathIgnorePatterns: ["/node_modules/", "/coverage/", "/dist/"],
+
+  // Make calling deprecated APIs throw helpful error messages
+  errorOnDeprecated: false,
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["js", "json", "jsx", "mdx"],
+
+  // Automatically restore mock state between every test
+  restoreMocks: false,
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ["src", "docs"],
+
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest"
+  }
+};
