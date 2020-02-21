643003cf2dac46685115338d8e8fc140ee406976
Colin de Roos <colinderoos@gmail.com>
2020-02-21 21:55:40 +0100

    Upgrade to docz 2🎉

--- doczrc.js
+++ doczrc.js
@@ -2,11 +2,13 @@
 export default {
   title: "React Turn Reveal",
   description: "A 3D reveal animation library for React.",
-  theme: "docz-theme-default",
   themeConfig: {
-    mode: "dark"
+    initialColorMode: "dark"
   },
-  menu: ["README", "TurnReveal", "FollowReveal"],
-  filterComponents: files =>
-    files.filter(filepath => /src\/.*\/*\.(js|jsx)$/.test(filepath))
+  menu: [
+    "Readme",
+    { name: "Components", menu: ["TurnReveal", "FollowReveal"] },
+    "Contributing",
+    "Changelog"
+  ]
 };
